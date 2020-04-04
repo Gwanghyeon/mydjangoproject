0fb6c965288d30dcfb91c67cdc2d9745bbb9e7ea
Gwanghyeon Gim <gwanghyeongim@Gwanghyeons-MacBook-Pro.local>
2020-04-04 20:45:41 +0900

    first

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+
+# Register your models here.
+admin.site.register(UserProfileInfo)
+
+# superuser info
+# username: admin
+# email: admin@email.com
+# password:testpassword
